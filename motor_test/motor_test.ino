97d8acd2134580cfa72a7c282326ea2413e83515
wed <samuelfong1301@gmail.com>
2025-09-10 17:19:24 +0800

    Update the comment

--- motor_test/motor_test.ino
+++ motor_test/motor_test.ino
@@ -1,30 +1,33 @@
 //Make sure there is a common ground between mcu and motor, otherwise you wouldn't be able to control the speed of the motor using pwm.
-//Also, connect VCC and GND of the motor using power supply, and set the voltage to ~7V.
+//To power the motor, connect the VCC and the GND  to the power supply, and turn the voltage to ~7V.
 
+//pins for motor and button
 #define WhitePin 16 // Clockwise/Anti-clockwise
 #define BluePin 17 //pwm speed control
 #define btn 3 //for testing only
 
 void setup() {
+  //make the pins outputs 
   pinMode(WhitePin, OUTPUT);
   pinMode(BluePin, OUTPUT);
   pinMode(btn, INPUT_PULLUP);
 }
 
 void loop() {
-  analogWrite(BluePin, 255); //full speed in 8bit analogWrite(default)
-  delay(1000);
-  analogWrite(BluePin, 127); //half speed in 8bit analogWrite(default)
-  delay(1000);
+  analogWrite(BluePin, 255); //control the motor at full speed using pwm
+  delay(1000); //wait for 1 second
+  analogWrite(BluePin, 127); //control the motor at half speed using pwm
+  delay(1000); //wait for 1 second
 }
 
 void loop1(){ //using 2nd core in rasberry pi pico 
+  //look if the button pressed
   if (digitalRead(btn) == 1){
-    digitalWrite(BluePin, LOW);
-    Serial.println(F("Anticlockwise"));
+    digitalWrite(BluePin, LOW); //turn the motor anti-clockwise
+    Serial.println(F("Anti-clockwise"));
   }
   else {
-    digitalWrite(BluePin, HIGH);
+    digitalWrite(BluePin, HIGH);//turn the motor clockwise
   }
 }
 
