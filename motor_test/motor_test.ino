8f3fdc3c7c21bb3e018858dc01c2faa18c7f4fe9
wed <samuelfong1301@gmail.com>
2025-09-17 13:00:30 +0800

    Update motor_test.ino:introduced move_straight function

--- motor_test/motor_test.ino
+++ motor_test/motor_test.ino
@@ -1,17 +1,24 @@
-//This is a example of motor testing
-//Make sure there is a common ground between mcu and motor, otherwise you wouldn't be able to control the speed of the motor using pwm.
-//To power the motor, connect the VCC and the GND  to the power supply, and turn the voltage to ~7V.
+/*This is a example of motor testing
+Make sure there is a common ground between mcu and motor, otherwise you wouldn't be able to control the speed of the motor using pwm.
+To power the motor, connect the VCC and the GND  to the power supply, and turn the voltage to ~7V.*/
 
 //pins for motor and button
-#define BluePin 17 //pwm speed control
-#define WhitePin 16
-#define btn 3 //for testing only
+const uint8_t BluePin1 = 17; //pwm speed control
+const uint8_t BluePin2 = 18; //pwm speed control
+const uint8_t WhitePin = 16; //pwm speed control
+const uint8_t btn = 3 //for testing only
+
+void move_straight(uint8_t speed_for_Motor1,uint8_t speed_for_motor2){
+	analogWrite(BluePin1,speed_for_Motor1);
+	analogWrite(BluePin2,speed_for_motor2);
+}
 
 void setup() {
   Serial.begin(9600);
   //make the pins outputs 
-  pinMode(BluePin, OUTPUT);
-  analogWrite(BluePin, 0); //control the motor at full speed using pwm
+  pinMode(BluePin1, OUTPUT);
+	pinMode(BluePin2, OUTPUT);
+	move_straight(0,0);
 }
 
 void setup1(){
@@ -21,6 +28,13 @@ void setup1(){
 }
 
 void loop() {
+	move_straight(0,0);//full speed
+	delay(2500);
+	move_straight(127,127);//half speed
+	delay(2500);
+	move_straight(255,255);//stop
+	delay(2500);
+/*
   analogWrite(BluePin, 127); //control the motor at half speed using pwm
   delay(2500); //wait for 2.5 second
   analogWrite(BluePin, 255);//stop
@@ -29,6 +43,7 @@ void loop() {
   delay(2500); //wait for 2.5 second
   analogWrite(BluePin, 255);//stop
   delay(2500); //wait for 2.5 second
+*/
 }
 
 
