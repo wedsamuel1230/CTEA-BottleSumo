379d2f77211a74f3ebda6860cd3261a6a168de5b
wici-kt <kadentong1@gmail.com>
2025-09-24 01:43:18 +0800

    Update sketch_sep12a.ino

--- sketch_sep12a/sketch_sep12a.ino
+++ sketch_sep12a/sketch_sep12a.ino
@@ -1,129 +1,88 @@
-/**************************************************************************
- This is an example for our Monochrome OLEDs based on SSD1306 drivers
-
- Pick one up today in the adafruit shop!
- ------> http://www.adafruit.com/category/63_98
-
- This example is for a 128x64 pixel display using I2C to communicate
- 3 pins are required to interface (two I2C and one reset).
-
- Adafruit invests time and resources providing this open
- source code, please support Adafruit and open-source
- hardware by purchasing products from Adafruit!
-
- Written by Limor Fried/Ladyada for Adafruit Industries,
- with contributions from the open source community.
- BSD license, check license.txt for more information
- All text above, and the splash screen below must be
- included in any redistribution.
- **************************************************************************/
-
-#include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_VL53L0X.h>
+#include "Adafruit_VL53L0X.h"
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Change to your chosen GPIO
+#define XSHUT_1 11 // GP11
+#define XSHUT_2 12 // GP12
+#define XSHUT_3 13 // GP13
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library. 
-// On an arduino UNO:       A4(SDA), A5(SCL)
-// On an arduino MEGA 2560: 20(SDA), 21(SCL)
-// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire1, OLED_RESET);
-
-Adafruit_VL53L0X lox = Adafruit_VL53L0X();
-
-void scanI2CDevices() {
-  byte error, address;
-  int nDevices = 0;
-  
-  Serial.println("Scanning...");
-  
-  for (address = 1; address < 127; address++) {
-    Wire1.beginTransmission(address);
-    error = Wire1.endTransmission();
-    
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address < 16)
-        Serial.print("0");
-      Serial.print(address, HEX);
-      Serial.println("  !");
-      
-      nDevices++;
-    }
-    else if (error == 4) {
-      Serial.print("Unknown error at address 0x");
-      if (address < 16)
-        Serial.print("0");
-      Serial.println(address, HEX);
-    }
-  }
-  
-  if (nDevices == 0)
-    Serial.println("No I2C devices found");
-  else
-    Serial.println("done");
-}
+Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox3 = Adafruit_VL53L0X();
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  while (!Serial);
+
   Wire1.setSDA(26);
   Wire1.setSCL(27);
-  Wire.setSDA(5);
-  Wire.setSCL(4);
   Wire1.begin();
-  Wire.begin();
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-  //setup for VL53L0X
-  Serial.println("Adafruit VL53L0X test");
-  if (!lox.begin(0x29,false,&Wire1)) {
-    Serial.println(F("Failed to boot VL53L0X"));
-    while(1);
-  }
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  // Clear the buffer
-  display.clearDisplay();
 
-  delay(1000);
-  display.setTextSize(2); // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(10, 0);
-  display.print("test");
-  display.display();
+  pinMode(XSHUT_1, OUTPUT);
+  pinMode(XSHUT_2, OUTPUT);
+  pinMode(XSHUT_3, OUTPUT);
 
-  while (!Serial); // Wait for serial monitor
-  Serial.println("I2C Scanner");
-  scanI2CDevices();
+  // Turn sensor OFF
+  digitalWrite(XSHUT_1, LOW);
+  digitalWrite(XSHUT_2, LOW);
+  digitalWrite(XSHUT_3, LOW);
+  delay(100);
+
+  // Change the TOF sensor Address
+
+  //Sensor 1
+
+  digitalWrite(XSHUT_1, HIGH);
+  delay(100);
+  lox1.begin(0x70,false,&Wire1);
+  delay(100);
+  scanI2C();
+  delay(100);
+  digitalWrite(XSHUT_1, LOW);
+  delay(100);
+
+  //sensor 2
+  digitalWrite(XSHUT_2, HIGH);
+  delay(100);
+  lox2.begin(0x71,false,&Wire1);
+  delay(100);
+  scanI2C();
+  delay(100);
+  digitalWrite(XSHUT_2, LOW);
+  delay(100);
+
+  //sensor3
+  digitalWrite(XSHUT_2, HIGH);
+  delay(100);
+  lox3.begin(0x72,false,&Wire1);
+  delay(100);
+  scanI2C();
+  delay(100);
+  digitalWrite(XSHUT_3, LOW);
+  delay(100);
+  
+  // Turn sensor back ON for normal operation
+  digitalWrite(XSHUT_1, HIGH);
+  digitalWrite(XSHUT_2, HIGH);
+  digitalWrite(XSHUT_3, HIGH);
 }
 
 void loop() {
-  VL53L0X_RangingMeasurementData_t measure;
-  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
-  display.clearDisplay();
-  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    display.setCursor(0,10);
-    display.print("Distance (mm): "); 
-    display.print(measure.RangeMilliMeter);
-    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
-  } else {
-    display.println(" out of range ");
-    Serial.println(" out of range ");
+  // do nothing
+}
+
+void scanI2C() {
+  byte error, address;
+  int nDevices = 0;
+  for (address = 1; address < 127; address++) {
+    Wire1.beginTransmission(address);
+    error = Wire1.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at 0x");
+      Serial.println(address, HEX);
+      nDevices++;
+    }
   }
-
-  display.display();
-  
-
-  delay(500);
-} 
+  if (nDevices == 0) Serial.println("No I2C devices found");
+  else Serial.println("done");
+}
